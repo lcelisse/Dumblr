8d83325bbd48a20a5c51e34a03628c987f815884
lilly <lcrhidalgo99@gmail.com>
2023-02-23 10:54:53 -0500

    delete

--- app/models/user.py
+++ app/models/user.py
@@ -15,9 +15,9 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
 
     posts = db.relationship(
-        "Post", back_populates="user", cascade="all,delete")
+        "Post", back_populates="user")
     comments = db.relationship(
-        "Comment", back_populates="user", cascade="all,delete")
+        "Comment", back_populates="user")
 
     @property
     def password(self):
