2d42b16c5cbf9d0d77392cf7f4e393016a6c38d6
lilly <lcrhidalgo99@gmail.com>
2023-03-18 22:27:28 -0400

    progress

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
@@ -25,67 +25,78 @@ function App() {
 
   return (
     <>
-      <Switch>
-        {!user?.id ? (
-          <switch>
-            <Navigation isLoaded={isLoaded} />
+      <BrowserRouter>
+        <Switch>
+          {!user?.id ? (
+            <switch>
+              <Navigation isLoaded={isLoaded} />{" "}
+              <Route exact path="/posts">
+                <Feed />
+              </Route>
+              <Route exact path="/">
+                <LandingPage />
+              </Route>
+              <Route exact path="/login">
+                <LoginFormPage />
+              </Route>
+              <Route exact path="/signup">
+                <SignupFormPage />
+              </Route>
+              <Route path="/">
+                <AboutUs />
+              </Route>
+              <Route path="/users/:userId">
+                <UserPage />
+              </Route>{" "}
+              <Route exact path="*">
+                {<PageNotFound />}
+              </Route>
+            </switch>
+          ) : (
+            <switch className="The-Body">
+              <Navigation isLoaded={isLoaded} />{" "}
+              <Route exact path="/posts">
+                <Feed />
+              </Route>
+              <Route exact path="/users/:userId/likes">
+                <LikesPage isLoaded={isLoaded} />
+              </Route>
+              <Route exact path="/users/following">
+                <Following isLoaded={isLoaded} />
+              </Route>
+              <Route exact path="/users/:userId">
+                <UserPage />
+              </Route>
+              <Route exact path="/">
+                <LandingPage />
+              </Route>{" "}
+              <Route path="/">
+                <AboutUs />
+              </Route>
+              <Route exact path="*">
+                {<PageNotFound />}
+              </Route>
+            </switch>
+          )}
+          <Navigation isLoaded={isLoaded} />
 
-            <Route exact path="/posts">
-              <Feed />
-            </Route>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
-            <Route exact path="/login">
-              <LoginFormPage />
-            </Route>
-            <Route exact path="/signup">
-              <SignupFormPage />
-            </Route>
-            <Route exact path="*">
-              <PageNotFound />
-            </Route>
-            <Route path="/">
-              <AboutUs />
-            </Route>
-          </switch>
-        ) : (
-          <div className="The-Body">
-            <Navigation isLoaded={isLoaded} />{" "}
-            <Route exact path="/posts">
-              <Feed />
-            </Route>
-            <Route exact path="/users/:userId/likes">
-              <LikesPage isLoaded={isLoaded} />
-            </Route>
-            <Route exact path="/users/following">
-              <Following isLoaded={isLoaded} />
-            </Route>
-            <Route path="/users/:userId">
-              <UserPage />
-            </Route>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>{" "}
-            <Route path="/">
-              <AboutUs />
-            </Route>{" "}
-            <Route exact path="*">
-              <PageNotFound />
-            </Route>
-          </div>
-        )}
-        <Navigation isLoaded={isLoaded} />
-        <Route path="/posts">
-          <Feed />
-        </Route>
-        <Route path="/users/:userId/likes">
-          <LikesPage isLoaded={isLoaded} />
-        </Route>
-        <Route path="/users/following">
-          <Following isLoaded={isLoaded} />
-        </Route>
-      </Switch>
+          <Route path="/posts">
+            <Feed />
+          </Route>
+          <Route path="/users/:userId/likes">
+            <LikesPage isLoaded={isLoaded} />
+          </Route>
+          <Route path="/users/following">
+            <Following isLoaded={isLoaded} />
+          </Route>
+          <Route exact path="/users/:userId">
+            <UserPage />
+          </Route>
+          <Route exact path="*">
+            {<PageNotFound />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
---
+++ react-app/src/components/UserPage/UserPage.js
@@ -0,0 +1,9 @@
+const UserPage = () => {
+  return (
+    <div className="user-page-container">
+      <div className="">hi</div>
+    </div>
+  );
+};
+
+export default UserPage;
