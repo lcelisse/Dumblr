1bd98620a651823519d47534d242c782d77c355b
lilly <lcrhidalgo99@gmail.com>
2023-04-17 14:13:22 -0400

    fix landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -4,6 +4,7 @@ import "./LandingPage.css";
 const LandingPage = () => {
   return (
     <div className="landing-page-container">
+      <h1 className="landing-heading">𝘚𝘐𝘎𝘕 𝘜𝘗 𝘖𝘙 𝘓𝘖𝘎 𝘐𝘕 𝘛𝘖 𝘊𝘙𝘌𝘈𝘛𝘌 𝘈 𝘗𝘖𝘚𝘛 !!</h1>
       <div className="image-container">
         <img className="background-landing-page" src={all}></img>
       </div>
--- react-app/src/components/LikesPage/LikesPage.js
+++ react-app/src/components/LikesPage/LikesPage.js
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { readUsersLikedPostThunk } from "../../store/post";
 import "./LikesPage.css";
+import PostPageComments from "../Comments/PostPageComments/PostPageComments";
 
 const LikesPage = ({ user, isLoaded }) => {
   const dispatch = useDispatch();
@@ -34,6 +35,45 @@ const LikesPage = ({ user, isLoaded }) => {
 export default LikesPage;
 
 const EachLikedPost = ({ each }) => {
+  const currentUser = useSelector((state) => state.session.user);
+  const [click, setClick] = useState(true);
+  const [clickComment, setClickComment] = useState("right-side-bottom");
+  const [clickLike, setClickLike] = useState(true);
+
+  const onCLickNote = () => {
+    if (currentUser !== null) {
+      click === false ? setClick(true) : setClick(false);
+      setClickLike(true);
+    } else {
+      click === true ? setClick(false) : setClick(true);
+      setClickLike(true);
+    }
+  };
+
+  const onCLickLike = () => {
+    if (currentUser !== null) {
+      clickLike === false ? setClickLike(true) : setClickLike(false);
+      setClick(true);
+    } else {
+      clickLike === true ? setClickLike(false) : setClickLike(true);
+      setClick(true);
+    }
+  };
+
+  let postLike;
+
+  const eachs = each.post_likes;
+  if (eachs && Object.values(eachs).length) {
+    const eachArr = Object.values(eachs);
+    postLike = eachArr.map((user) => (
+      <div className="display-username-of-likes">
+        {/* <Link className="link-for-username" to={`/users/${user?.id}`}> */}
+        {user?.username}
+        {/* </Link> */}
+      </div>
+    ));
+  }
+
   return (
     <div className="each-liked-post-container">
       <div className="container-for-each">
@@ -48,6 +88,35 @@ const EachLikedPost = ({ each }) => {
           <img className="image-for-each-post" src={each.url} />
         </div>
         <div className="each-title-container">{each.body}</div>
+        <nav className="nav-for-comment-stuff">
+          <p className="notes-label">
+            <p className="notes-label-text" onClick={onCLickNote}>
+              Notes
+            </p>{" "}
+          </p>
+        </nav>
+        <div
+          className={click ? "logged-out-comments" : "logged-comments-clicked"}
+        >
+          {" "}
+          <div
+            className={
+              clickLike
+                ? "show-likes-for-post-drop-down"
+                : "show-likes-for-down-clicked"
+            }
+          >
+            {postLike}
+          </div>
+          <div>
+            <PostPageComments
+              eachPost={each}
+              click={click}
+              setClickComment={setClickComment}
+              clickComment={clickComment}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
